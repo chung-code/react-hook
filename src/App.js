52ae2db7b7010703ab4e04fa3385068ab960a157
chung <qc160@naver.com>
2021-03-30 00:06:24 +0900

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component, useState, useEffect} from 'react'
 import './App.css';
 import List from './List.jsx';
+import useFetch from './useFetch.js'
+
 
 const App = () => {
-  const [todos, setTodos] = useState(['js공부']);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState();
+
+  const loading = useFetch(setTodos, 'http://localhost:8080/todo'); //분리된 customhook 사용
 
   const changeInputData = (e) =>{
     setNewTodo(e.target.value); //setNewTodo: 입력된 새로운 값(변수 or 함수로 정의 가능)을 newTodo에 넣어준다
@@ -12,7 +16,7 @@ const App = () => {
 
   const addTodo = (e) => {
     e.preventDefault(); //form 안에 submit 역할을 하는 button을 눌러도 창 전체가 동기화 되지 않도록
-    setTodos([...todos, newTodo]); //setTodos: 원래의 todos 배열에 새롭게 갱신된 newTodo 정보를 추가하여 todos에 넣어준다
+    setTodos([...todos, {'title': newTodo, 'id': todos.length, 'status': 'todo'}]); //setTodos: 원래의 todos 배열에 새롭게 갱신된 newTodo 정보를 추가하여 todos에 넣어준다
   }
 
   useEffect( ()=> {
@@ -28,7 +32,7 @@ const App = () => {
       <button onClick={addTodo}>할일추가</button>
     </form>
 
-    <List todos={todos}/>
+    <List todos={todos} loading={loading}/>
     </>
   )
 }
