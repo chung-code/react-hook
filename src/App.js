cb292eba0b685d480c87982d81fb682dd9cc14e3
chung <qc160@naver.com>
2021-03-31 01:14:00 +0900

    changetodostatus

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component, useState, useEffect} from 'react'
 import './App.css';
 import List from './List.jsx';
-import useFetch from './useFetch.js'
+import useFetch from './useFetch.js';
+import Header from './Header.jsx';
 
 
 const App = () => {
@@ -19,20 +20,32 @@ const App = () => {
     setTodos([...todos, {'title': newTodo, 'id': todos.length, 'status': 'todo'}]); //setTodos: 원래의 todos 배열에 새롭게 갱신된 newTodo 정보를 추가하여 todos에 넣어준다
   }
 
+  const changeTodoStatus = (id) => {
+    // debugger; 디버깅을 통해서 id가 문자열로 반환하는 것을 발견 -> 두번째 아랫줄에 숫자로 변경
+    const updateTodos = todos.map(todo => {
+      if(todo.id === +id) {
+        if(todo.status === "done") todo.status = "todo";
+        else todo.status = "done";
+      }
+      return todo;
+    })
+    setTodos(updateTodos);
+  }
+
   useEffect( ()=> {
     console.log("새로운 내용이 렌더링됐네요");
   },[todos]) //렌더링 시점을 명시하기 위한 관찰할 state 지정
 
   return (
     <>
-    <h1>todo 애플리케이션</h1>
+    <Header todos={todos}/>
 
     <form action="">
       <input type="text" name="" onChange={changeInputData}/>
       <button onClick={addTodo}>할일추가</button>
     </form>
 
-    <List todos={todos} loading={loading}/>
+    <List todos={todos} loading={loading} changeTodoStatus={changeTodoStatus}/>
     </>
   )
 }
