d3ae93d7e21af9cfb2baa08cedccbcd7cb890919
chung <qc160@naver.com>
2021-04-05 23:31:54 +0900

    useRef

--- src/TodoStore.js
+++ src/TodoStore.js
@@ -9,16 +9,11 @@ export const TodoContext = React.createContext();
 
 const TodoStore = () => {
   const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState();
 
   const loading = useFetch(setTodos, 'http://localhost:8080/todo'); //분리된 customhook 사용
 
-  const changeInputData = (e) =>{
-    setNewTodo(e.target.value); //setNewTodo: 입력된 새로운 값(변수 or 함수로 정의 가능)을 newTodo에 넣어준다
-  }
 
-  const addTodo = (e) => {
-    e.preventDefault(); //form 안에 submit 역할을 하는 button을 눌러도 창 전체가 동기화 되지 않도록
+  const addTodo = (newTodo) => {
     setTodos([...todos, {'title': newTodo, 'id': todos.length, 'status': 'todo'}]); //setTodos: 원래의 todos 배열에 새롭게 갱신된 newTodo 정보를 추가하여 todos에 넣어준다
   }
 
@@ -39,7 +34,7 @@ const TodoStore = () => {
   },[todos]) //렌더링 시점을 명시하기 위한 관찰할 state 지정
 
   return (
-    <TodoContext.Provider value={{todos, addTodo, changeInputData, loading, changeTodoStatus}}>
+    <TodoContext.Provider value={{todos, addTodo, loading, changeTodoStatus}}>
       <Header />
 
       <Form />
